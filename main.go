49eaae75da2ebefd85d8b401de5433a0d2256759
Thierry Rakotomanana <ThierryRktm@gmail.com>
2022-12-08 11:27:10 +0300

    fix: name of the veiw to render

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/", &templateHandler{filename: "dateTemplate.html"})
 	// start the web server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
